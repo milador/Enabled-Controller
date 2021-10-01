c31cbc987f15dacfddb4d6d0a8e2a4bcb3c064df
milador <milador.tech@gmail.com>
2021-09-30 19:12:17 -0700

    Updated all files to version 1.2

--- Software/Arduino/Enabled_Controller_Wireless_Software/Enabled_Controller_Config.h
+++
@@ -1,9 +0,0 @@
-// COMMON SETTINGS
-// ----------------------------------------------------------------------------------------------
-// These settings are used in both SW UART, HW UART and SPI mode
-// ----------------------------------------------------------------------------------------------
-
-#define BLUEFRUIT_DEVICE_NAME               "Enabled-Controller"    
-#define BLUEFRUIT_DEVICE_MANUFACTURER       "Milador"  
-#define BLUEFRUIT_DEVICE_MODEL              "Switch-Interface"  
-#define BLUEFRUIT_DEVICE_HID                BLE_APPEARANCE_HID_KEYBOARD && BLE_APPEARANCE_HID_MOUSE 
